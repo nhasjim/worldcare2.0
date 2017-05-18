9976bf9a5f57968a609e868f2e41a369b7e8668f
Ori Gilad <ogilad@ucsd.edu>
2017-05-17 18:49:00 -0700

    Hopefully this works

--- gulpfile.js
+++ gulpfile.js
@@ -88,8 +88,14 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+//    gulp.watch('*.html', browserSync.reload);
+  //  gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+gulp.task('heroku:production', ['less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
 });
 
 // Compiles SCSS files from /scss into /css
